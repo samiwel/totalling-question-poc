a27952463ee291be628724f695ff9912664845df
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-01-06 15:20:01 +0000

    Initial commit.

---
+++ totaliser.js
@@ -0,0 +1,39 @@
+function answerChanged() {
+  var newTotal = 0;
+
+  var percentages = document.getElementsByClassName('percentage');
+  var count = percentages.length
+
+  var i
+  for (i=0; i < count; i++) {
+    var percentage = percentages[i].value
+    if (percentage === '') {
+      continue;
+    }
+
+    var value = parseInt(percentage)
+    if (value != 'NaN') {
+      newTotal += value
+    }
+  }
+
+  updateTotal(newTotal)
+}
+
+function updateTotal(value) {
+  document.getElementById('total').value = value
+  var indicator = document.getElementById('total-container')
+  if (value > 100 || value < 0)  {
+    // bad
+    indicator.classList.remove('good')
+    indicator.classList.add('bad')
+  } else if (value == 100) {
+    // good
+    indicator.classList.remove('bad')
+    indicator.classList.add('good')
+  } else if (value < 100 && value >= 0) {
+    // normal
+    indicator.classList.remove('good')
+    indicator.classList.remove('bad')
+  }
+}
